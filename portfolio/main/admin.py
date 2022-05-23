ce60bcc57e47734b27484ed54e227248abd3aac6
tspyrk <krypst@gmail.com>
2022-05-23 22:59:02 +0000

    v2

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from . models import Operation
-from . models import Active
+from .models import Operation
+from .models import Active
 
-# Register your models here.
 admin.site.register(Operation)
 admin.site.register(Active)
-# admin.site.register(Active2)
